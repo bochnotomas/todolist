f7048e212ac2fe06bc22714190e97556229ee562
bochnotomas <tomasbochnovic76@gmail.com>
2021-03-18 23:20:52 +0000

    Project setup done

---
+++ src/index.js
@@ -0,0 +1,10 @@
+class Item {
+    constructor(title, description, dueDate, priority, checked) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.checked = checked;
+    }
+
+}
